9c95b9d97d17cfc236aeb1840602f6adcb09576c
lsamo02 <samotis@wisc.edu>
2023-12-14 10:59:03 -0500

    Lab 2 Attempt 1

--- js/main_working.js
+++ js/main_working.js
@@ -4,6 +4,22 @@
     var attrArray = ["Country_Name","Country_Code","1990","1995","2000","2005", "2010","2015", "2020"]; //list of attributes
     var expressed = attrArray[2]; //initial attribute
 
+        //chart frame dimensions
+    var chartWidth = window.innerWidth * 0.425,
+        chartHeight = 473,
+        leftPadding = 25,
+        rightPadding = 2,
+        topBottomPadding = 5,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding * 2,
+        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+    //create a scale to size bars proportionally to frame and for axis
+// Create a scale to size bars proportionally to frame
+    var yScale = d3.scaleLinear()
+        .range([chartInnerHeight, 0])
+        .domain([0, 35]);
+
     window.onload = setMap;
 
     //set up choropleth map
@@ -50,6 +66,7 @@
             console.log(backgroundCountries);
             console.log('look at me', top15MostPopulousCountries);
 
+            
             //add Europe countries to map
             var countries = map.append("path")
                 .datum(backgroundCountries)
@@ -64,7 +81,17 @@
                 .attr("class", function(d){
                     return "regions " + d.properties.Country_Code;
                 })
-                .attr("d", path);
+                .attr("d", path)
+                .on('mouseover', function (d) {
+                    highlight(d.properties);
+                })
+                .on('mouseout', function (d) {
+                    dehighlight(d.properties);
+                })
+                .on('mousemove', function () {
+                    moveLabel();
+                });
+
 
             top15MostPopulousCountries = joinData(top15MostPopulousCountries, csvData);
 
@@ -75,6 +102,63 @@
             regions.style("fill", function(d) {
                 return colorScale(d.properties[expressed]);
             });
+
+            // Add coordinated visualization (bar chart) to the map
+            setChart(csvData, colorScale);
+
+
+                        //Example 1.1 line 1...function to create a dropdown menu for attribute selection
+            function createDropdown(csvData){
+                //add select element
+                var dropdown = d3.select("body")
+                    .append("select")
+                    .attr("class", "dropdown")
+                    .on("change", function(){
+                        changeAttribute(this.value, csvData)
+                    });
+
+
+                //function to create a dropdown menu for attribute selection
+            function createDropdown(){
+                //add select element
+                var dropdown = d3.select("body")
+                    .append("select")
+                    .attr("class", "dropdown");
+
+                //add initial option
+                var titleOption = dropdown.append("option")
+                    .attr("class", "titleOption")
+                    .attr("disabled", "true")
+                    .text("Select Attribute");
+
+                //add attribute name options
+                var attrOptions = dropdown.selectAll("attrOptions")
+                    .data(attrArray)
+                    .enter()
+                    .append("option")
+                    .attr("value", function(d){ return d })
+                    .text(function(d){ return d });
+            };
+
+            
+            //dropdown change event handler
+            function changeAttribute(attribute, csvData) {
+                //change the expressed attribute
+                expressed = attribute;
+
+                //recreate the color scale
+                var colorScale = makeColorScale(csvData);
+
+                //recolor enumeration units
+                var regions = d3.selectAll(".regions").style("fill", function (d) {
+                    var value = d.properties[expressed];
+                    if (value) {
+                        return colorScale(d.properties[expressed]);
+                    } else {
+                        return "#ccc";
+                    }
+                });
+            }
         }
 
         function makeColorScale(data){
@@ -102,6 +186,7 @@
 
             return colorScale;
         }
+    };
 
         function joinData(top15MostPopulousCountries, csvData){
             // Loop through CSV data to assign each set of CSV attribute values to GeoJSON countries
@@ -129,4 +214,195 @@
         }
         
     }
+
+    // Function to create the coordinated bar chart
+    function setChart(csvData, colorScale) {
+        // Chart frame dimensions
+        var chartWidth = window.innerWidth * 0.325,
+            chartHeight = 500;
+
+        // Create a second SVG element to hold the bar chart
+        var chart = d3.select("#chart-container")
+            .append("svg")
+            .attr("width", chartWidth)
+            .attr("height", chartHeight)
+            .attr("class", "chart");
+
+        // Find the maximum data value for the expressed attribute to set up the yScale domain
+        var maxVal = d3.max(csvData, function(d) { return parseFloat(d[expressed]); });
+
+
+        // Create a scale to size bars proportionally to frame
+        var yScale = d3.scaleLinear()
+            .range([0, chartHeight])
+            .domain([0, maxVal]); //changed this from 105 to maxVal
+
+        // Set bars for each province
+        var bars = chart.selectAll(".bar")
+            .data(csvData)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("width", chartWidth / csvData.length - 1)
+            .attr("x", function(d, i){
+                return i * (chartWidth / csvData.length);
+            })
+            .attr("height", function(d){
+                return yScale(parseFloat(d[expressed]));
+            })
+            .attr("y", function(d){
+                return chartHeight - yScale(parseFloat(d[expressed]));
+            })
+            .style("fill", function(d){
+                return colorScale(d[expressed])
+            })
+            .attr('transform', translate)
+            .on("mouseover",highlight)
+            .on("mouseover", dehighlight)
+
+        
+        var numbers = chart.selectAll(".numbers")
+            .data(csvData)
+            .enter()
+            .append("text")
+            .sort(function(a, b){
+                return b[expressed]-a[expressed]
+            })
+            .attr("class", function(d){
+                return "numbers " + d.Country_Code;
+            })
+            .attr("text-anchor", "middle")
+        
+            .attr("x", function(d, i){
+                var fraction = chartWidth / csvData.length;
+                return i * fraction + (fraction - 1) / 2;
+            })
+            .attr("y", function(d){
+                return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+            })
+            .text(function(d){
+                return d[expressed];
+
+
+            });
+
+        var chartTitle = chart.append("text")
+            .attr("x", 80)
+            .attr("y", 20)
+            .attr("class", "chartTitle")
+            .text("This Country's Total Forested Area (sq km) is " + expressed[3]);
+    }
+
+    function createDropdown(csvData) {
+        let dropdown = d3.select('body')
+            .append('select')
+            .attr('class', 'dropdown')
+            .on('change', function () {
+                changeAttribute(this.value, csvData)
+            });
+    
+        //add initial option
+        let titleOption = dropdown.append('option')
+            .attr('class', 'titleOption')
+            .attr('disabled', 'true')
+            .text('Select Attribute');
+    
+        // Add attribute name options
+        let attrOptions = dropdown.selectAll('attrOptions')
+            .data(attrArray)
+            .enter()
+            .append('option')
+            .attr('value', function (d) {
+                return d
+            })
+            .text(function (d) {
+                return d
+            });
+    }
+
+    
+    // Function to highlight states and bars
+    function highlight(csvData) {
+        // Change stroke
+        var selected = d3.selectAll('.' + Country_Code)
+            .style('stroke', '#c8c8c8')
+            .style('stroke-width', '2');
+
+        setLabel(props);
+    }
+
+    //function to reset the element style on mouseout
+    function dehighlight(csvData) {
+        var selected = d3.selectAll("." + Country_Code)
+            .style("stroke", "black")
+            .style("stroke-width", "0.5px");
+
+        d3.select(".infolabel")
+            .remove();
+    };
+
+    function highlight(d) {
+        d3.select()
+            .style("stroke","black")
+            .style("stroke-width","2px");
+    }
+
+    function dehighlight(d) {
+        d3.select()
+            .style("stroke","none")
+            .style("stroke-width","0px");
+    }
+
+    //function to create dynamic label
+function setLabel(props) {
+    //label content
+    
+    var classState = "<p><b>" + props[expressed + "_Count"] + "</b><small> gyms offer " + expressed + " in <b>" + props.postal + "</b></small></p>";
+    var count = "<p>(<b>" + props[expressed] + "</b><small> gyms per million people)</small></p>";
+    var labelAttribute = classState + count;
+
+    //create info label div
+    var infolabel = d3.select("body")
+        .append("div")
+        .attr("class", "infolabel")
+        .attr("id", props.postal + "_label")
+        .html(labelAttribute);
+
+    var regionName = infolabel.append("div")
+        .attr("class", "labelname")
+        .html(props.name);
+};
+
+//function to move info label with mouse
+function moveLabel() {
+    //get width of label
+    var labelWidth = d3.select(".infolabel")
+        .node()
+        .getBoundingClientRect()
+        .width;
+    
+    var labelHeight = d3.select(".infolabel")
+        .node()
+        .getBoundingClientRect()
+        .height;
+    
+    //use coordinates of mousemove event to set label coordinates    
+    var x1 = d3.event.pageX + 10,
+        y1 = d3.event.pageY - labelHeight - 10,
+        x2 = d3.event.pageX - labelWidth - 5,
+        y2 = d3.event.pageY + 25;
+
+    //horizontal label coordinate, testing for overflow
+    var x = d3.event.pageX > window.innerWidth - labelWidth - 20 ? x2 : x1;
+    //vertical label coordinate, testing for overflow
+    var y = d3.event.pageY < 75 ? y2 : y1;
+
+    d3.select(".infolabel")
+        .style("left", x + "px")
+        .style("top", y + "px");
+};
+
+
+
+
 })(); //last line
